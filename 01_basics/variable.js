f10eaa35e37a9cd181e3360f5255592f14d8cc83
Ayush Mishra <nitinmishra6267@gmail.com>
2026-07-20 01:38:27 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12234
+let accountEmail="ayush@gmail.com"
+var accountPassword="1654547"
+accountCity="India"
+
+accountEmail = "newemail@gmail.com"
+accountPassword="122"
+accountCity="USA"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
